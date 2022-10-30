69cb4c62e9d0e7920d8a855a429b5928571d965a
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-29 22:22:33 -0300

    inserindo_'css'_inline_externo_e_classe_no_documento_'react'

---
+++ css_react/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+
+export default function App() {
+
+  const textoDestaque = {   // constante com formatação espeçifica
+    color: 'blue',
+    fontSize: '2em'
+  }
+
+  return(
+    <>
+    <section className='caixa'>
+        <h1 style={{color: 'red', fontSize: '3em'}}>Análise e desenvolvimento de sistemas</h1>   {/* 'css' inline */}
+        <h2 style={textoDestaque}>Técnico de informatica para internet</h2>   {/* ultilizando 'constate' para formatação */}
+        <h3 className='texto3'>Técnico de Eletroeletrônica</h3>
+        <h4 className='texto4'>Lógica de programação</h4>
+        <h5 className='texto5'>Front end</h5>
+      </section>
+    </>
+  )
+}
