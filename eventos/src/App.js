b774f62af980c4af1f43b74865054742c0c03691
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-11-03 01:05:44 -0300

    cancelando_a_acao_padrao_de_link_com_'preventDefault()'_do_proprio_componente

--- eventos/src/App.js
+++ eventos/src/App.js
@@ -4,9 +4,19 @@ export default function App() {
 
   const [ligado, setLigado] = useState(false);    // criação do 'holk'
 
+  const cancelar =(obj)=>{
+    return obj.preventDefault()
+  }
+
   return(
     <>
       <Led ligado= {ligado} setLigado = {setLigado}/>
+      <a href='https://github.com/Henricknen/React'
+      target='blank'
+      onClick={(e)=>cancelar(e)}    // chamando a unção 'cncelar()'
+      >
+        Repositorio de REACT
+      </a>
     </>
   );
 }
