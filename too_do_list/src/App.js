313fcc89dfa75e02b12da17d4bea1a5bac05b566
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-11-10 15:40:30 -0300

    finalizando_o_'too_do_list'_e_criando_funcao_para_'deletar'_tarefas_adicionadas

--- too_do_list/src/App.js
+++ too_do_list/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,19 +6,48 @@ function App() {
 let [lista, setLista] = useState(["Tarefa 1", "Tarefa 2", "Tarefa 3", "Tarefa 4"]);    // 'useState' vai retorna o estado e a função de alteera o valor da lista de tarefas
 let [novoItem, setNovoItem] = useState("") // outro 'estado'
 
+useEffect(() => {   // 'hook' com uma função com array de dependeçia vazio que ira executa a função apenas uma vez
+  setLista(["Tarefa1", "Tarefa2", "Tarefa3", "Tarefa4", "Tarefa5", ])
+}, []);   // '[]' array de dependeçia vazio
+
 return(
-  <div>
-    <input value={novoItem} onChange={value => setNovoItem(value.target.value)} type="text"/>   
+  <div className='container'>
+    <input placeholder='Tarefas' value={novoItem} onChange={value => setNovoItem(value.target.value)} type="text"/>   
     <button onClick={() => adicionarNovoItem()}>Adicionar</button>
-      <ul>
-        {lista.map(item => <li>{item}</li>)}
+      <ul className='todo-list'>
+        {lista.map((item,index) => (
+        <li key = {index} className='todo-item'>
+          {item}
+          <button onClick={() => deletarItem(index)}>
+            Deletar
+            </button>
+          </li>
+          ))}
       </ul>
   </div>
   );
 
   function adicionarNovoItem() {
+    if(novoItem.length <= 0) {
+      alert("Digite alguma tarefa...");
+      return;
+    }
+
+    let itemIndex = lista.indexOf(novoItem);
+    if(itemIndex >= 0) {
+      alert("Esta tarefa ja está adicionada");
+      return;
+    }
+
     setLista([...lista, novoItem]);   // adiciona novo intem em baixo dos itens ja existentes
     setNovoItem("");    // zera o input depois de item adicionadao
+  }
+
+  function deletarItem(index) {
+    let tmpArray = [...lista];
+    tmpArray.splice(index, 1);
+
+    setLista(tmpArray);
   }
 }
 
