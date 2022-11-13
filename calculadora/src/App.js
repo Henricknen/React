22a5546ebb7e0a2a5bf36c871dbce5d66df325dd
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-11-13 20:40:29 -0300

    finalizando_calculadora_matematica_criada_em_'react'

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -7,12 +7,25 @@ export default function App() {
   const[acumulador, setAcumulador] = useState(0)
   const[operado, setOperado] = useState(false)
 
+//  Componentes
   const Tela =(valor, res)=> {
     return(
-      <div style = "cssTela">
+      <div style = {cssTela}>
         <span style={cssTelaOper}>{valor}</span>
         <span style={cssTelaRes}>{res}</span>
       </div>
+    )
+  }
+
+  // const Btn=(label, onClick)=> {
+  //   return(
+  //     <button style={cssBtn} onClick>{label}</button>
+  //   )
+  // }
+
+  const Btn = (label,onClick) => {
+    return(
+      <button style={cssBtn} onClick={onClick}>{label}</button>
     )
   }
 
@@ -26,7 +39,7 @@ export default function App() {
     }
 
     if(operado) {
-      setValorTela(d),
+      setValorTela(d)
       setOperado(false)
       return
     }
@@ -35,51 +48,79 @@ export default function App() {
     setValorTela(valorDigitadoTela)
   }
 
-  const imparMemoria=()=> {
-    setOperado(false),
-    setValorTela(''),
-    setResultado(0),
+  const limparMemoria=()=> {
+    setOperado(false)
+    setValorTela('')
+    setResultado(0)
     setAcumulador(0)
     return
   }
 
+  const Operacao=(oper)=> {
+    if(oper == 'bs') {
+      let vtela = valorTela
+      vtela = vtela.substring(0, (vtela.length - 1))
+      setValorTela(vtela)
+      setOperado(false)
+      return
+    }
+
+    try {   // Calculo
+      const r = eval(valorTela)
+      setAcumulador(r)
+      setResultado(r)
+      setOperado(true)
+    } catch {
+      setResultado('Error')
+    }
+  }
+  
+
   // Estilo
+  const cssConteiner = {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flexDirection: 'column',
+    width: 300,
+    border: '1px solid #000'
+  }
+
+  const cssBotoes = {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  }
+
   const cssTela = {
-    displa: 'flex',
+    display: 'flex',
     paddingLeft: 20,
     paddingRight: 20,
     justifyContent: 'center',
     alignItems: 'flex-start',
     backgroundColor: '#444',
     flexDirection: 'column',
-    window: 260
+    width: 260
   }
 
-  const Btn = (label, onClick) => {
-    return(
-      <button style={cssBtn} onClick={onClick}>{label}</button>
-    )
-  }
 
   const cssTelaOper = {
-    FontSize: 25,
+    fontSize: 25,
     color: '#fff',
     height: 20
   }
 
   const cssTelaRes = {
-    FontSize: 50,
-    color: '#fff',
-    height: 20
+    fontSize: 50,
+    color: '#fff'
   }
 
   const cssBtn = {
     fontSize: 30,
     height: 75,
-    with: 75,
+    width: 75,
     padding: 20,
     backgroundColor: '#000',
-    color: '3fff',
+    color: '#fff',
     boderColor: '#000',
     textAlign: 'center',
     outLine: 'none'
@@ -88,6 +129,34 @@ export default function App() {
 
   return(
     <>
+      <div style={cssConteiner}>
+        <h3>Calculadora em React</h3>
+        {Tela(valorTela,resultado)}
+        <div style={cssBotoes}>
+          {Btn('AC', limparMemoria)}
+          {Btn('(',()=> addDigitoTela('('))}
+          {Btn(')',()=> addDigitoTela(')'))}
+          {Btn('/',()=> addDigitoTela('/'))}
+          {Btn('7',()=> addDigitoTela('7'))}
+          {Btn('8',()=> addDigitoTela('8'))}
+          {Btn('9',()=> addDigitoTela('9'))}
+          {Btn('*',()=> addDigitoTela('*'))}
+          {Btn('4',()=> addDigitoTela('4'))}
+          {Btn('5',()=> addDigitoTela('5'))}
+          {Btn('6',()=> addDigitoTela('6'))}
+          {Btn('-',()=> addDigitoTela('-'))}
+          {Btn('1',()=> addDigitoTela('1'))}
+          {Btn('2',()=> addDigitoTela('2'))}
+          {Btn('3',()=> addDigitoTela('3'))}
+          {Btn('+',()=> addDigitoTela('+'))}
+          {Btn('0',()=> addDigitoTela('0'))}
+          {Btn('.',()=> addDigitoTela('.'))}
+          {Btn('<-',()=> Operacao('bs'))}
+          {Btn('=',()=> Operacao('='))}
+
+        </div>
+
+      </div>
     
     </>
   );
