29c6fb855f73af52c465359ce3494c1fd8437ffe
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-11-17 16:18:15 -0300

    fazendo_a_manipulacao_de_varios_elemento_do_formulario_com_apenas_um_'state'

--- manipulando_objetos_com_state/src/App.js
+++ manipulando_objetos_com_state/src/App.js
@@ -2,16 +2,25 @@ import React, {useState} from 'react'   // fazendo import do ''useState'
 
 export default function App() {
 
-  const [form, setForm] = useState({"nome":"Luis henrique s f", "graduacao":"Analise e desenvolvimento de sistemaas", "ano":"2023"})       // passando os objetos 'nome', 'grauacao' e 'ano', iniçializados  
+  const [form, setForm] = useState({"nome":"", "graduacao":"", "ano":""})       // passando os objetos 'nome', 'grauacao' e 'ano', iniçializados  
+  const handlFormChange=(e)=> {    // função manipuladora 'handl' com elemento 'e'
+    if(e.target.getAttribute('name') == 'fnome' ) {
+      setForm({"nome":e.target.value, "graduacao":form.graduacao, "ano":form.ano})
+    } else if(e.target.getAttribute('name') == 'fgraduacao' ) {
+      setForm({"nome":form.nome, "graduacao":e.target.value, "ano":form.ano})
+  } else if(e.target.getAttribute('name') == 'fano' ) {
+    setForm({"nome":form.nome, "graduacao":form.graduacao, "ano":e.target.value})
+  }
+}
 
   return (
     <>
       <label>Nome:</label> 
-      <input type="text" name="fnome" value={form.nome} onChange=""/><br/>    
+      <input type="text" name="fnome" value={form.nome} onChange={(e)=>handlFormChange(e)}/><br/>    
       <label>Graduação:</label> 
-      <input type="text" name="fgraduacao" value={form.graduacao} onChange=""/><br/>
+      <input type="text" name="fgraduacao" value={form.graduacao} onChange={(e)=>handlFormChange(e)}/><br/>
       <label>Ano:</label> 
-      <input type="text" name="fano" value={form.ano} onChange=""/><br/>
+      <input type="text" name="fano" value={form.ano} onChange={(e)=>handlFormChange(e)}/><br/>
 
       <p>Nome digitado: {form.nome}</p>   {/* pegando os valores dos 'state'*/}
       <p>Graduação digitada: {form.graduacao}</p>
@@ -20,6 +29,3 @@ export default function App() {
     </>
   );
 }
-
-
-
