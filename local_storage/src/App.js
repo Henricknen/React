490e66bbee85f1e7c1f2a001cd0e8fbf0100fba5
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-11-25 12:18:53 -0300

    manipulando_'localStorage'_em_react_criando_uma_chave_com_um_valor_alterando_o_valor_da_mesma_,_consultando_o_valor_dela_e_fazendo_a_remocao_da_chave_existente

--- local_storage/src/App.js
+++ local_storage/src/App.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'   // imoprtando 'useState'
 
 export default function App() {
 
-  localStorage.setItem("nome", "LuisHenriqueSF")   //'localStorage' vai cria a chave chamada 'nome' com valor 'LuisHenriqueSF'
-  localStorage.getItem("nome")    // recuperando a chave
-  localStorage.removeItem("nome")   // removendo chave
+  const [nome, setNome] = useState()    // criação de 'state' chamado nome vazio
+
+  const armazenar =(chave, valor)=> {       // arrow function 'armazenar'
+    localStorage.setItem(chave, valor)
+  }
+
+  const consutar =(chave)=> {   // arrow function 'consultará' uma chave
+    alert(localStorage.getItem(chave))
+  }
+
+  const apagar =(chave)=> {   // arrow function 'apagará' uma chave
+    localStorage.removeItem(chave)
+  }
 
   return(
     <>
+      <label>Digite seu Nome:</label><br/>
+      <input type="text" value={nome} onChange={(e)=>setNome(e.target.value)}/><br/>    {/* 'input text' associado com oa state nome por meio do 'value={nome}' */}
+      <button onClick={()=>armazenar('ls_nome', nome)}>Gravar nome</button>    {/* botão cham a função armazenar */}
+      <button onClick={()=>consutar('ls_nome')}>Consutar nome</button>    {/* botão consultar recebe o parametrô ls_nome' que é a chave */}
+      <button onClick={()=>apagar('ls_nome')}>Remover nome</button>
        
     </>
   );
