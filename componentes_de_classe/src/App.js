a026db087ced8630d781d1bf9a58dabeef9bf20b
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-11-25 13:34:59 -0300

    criacao_do_primeiro_'componente_de_classe'

---
+++ componentes_de_classe/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Classe from './componentes/Classe'
+
+export default function App() {
+
+  return(
+    <>
+      <h1>Componente de classe</h1>
+      <Classe/>
+    </>
+  );
+}
---
+++ componentes_de_classe/src/componentes/Classe.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Classe extends React.Componet {       // classe 'extends' herdando de React.Componet
+    render(){        // chamando o mérodo 'render'
+        return(
+            <h1>Primeiro Componente de Classe</h1>       // componente em modo de classe
+        )
+    }   
+}
