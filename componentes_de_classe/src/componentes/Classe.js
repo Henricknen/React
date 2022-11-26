e89daf31f832475de513d72f2e8cef436fecd000
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-11-26 08:58:06 -0300

    passando_propriedade_e_capturando-a_dentro_do_componente_classe

--- componentes_de_classe/src/componentes/Classe.js
+++ componentes_de_classe/src/componentes/Classe.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-export default class Classe extends React.Componet {       // classe 'extends' herdando de React.Componet
-    render(){        // chamando o mérodo 'render'
+export default class Classe extends React.Componet {      // classe 'extends' herdando de React.Componet
+constructor(props) {     // método contrutor 'props' pega as propriedades que estão sendo passada pelo objeto instanciado
+    super(props)            // 'super' chama o construtor da classe pai
+}
+    render(){        // chamando o método 'render'
         return(
-            <h1>Primeiro Componente de Classe</h1>       // componente em modo de classe
+            <div>
+                <h1>Primeiro Componente de Classe</h1>       // componente em modo de classe
+                <p>Repositorio: {props.repositorio}</p>
+                <p>Me chamo: {props.nome}</p>
+            </div>
         )
     }   
 }
